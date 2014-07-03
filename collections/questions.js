4edaa8295ddd79c8af3b16c62e237b52527772e0
Jeff Lau <jeff-lau@live.com>
2014-07-04 03:35:28 +0800

    Completed basic read-mode of notice board

--- collections/questions.js
+++ collections/questions.js
@@ -3,5 +3,8 @@ Questions = new Meteor.Collection('questions');
 Questions.allow({
   insert: function(){
     return true;
+  },
+  remove: function(){
+    return true;
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -10,6 +10,10 @@ Meteor.publish('questions', function(){
   return Questions.find();
 });
 
+Meteor.publish('singleQuestion', function(id) {
+  return id && Questions.find(id);
+});
+
 Meteor.publish('courses', function() {
   return Courses.find();
 });
